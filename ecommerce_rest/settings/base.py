325bb8610a9df86ab1909d349b6dd842c5c841a2
EmanuelCasta <emanuel.castaneda.cardona@gmail.com>
2022-09-25 18:37:59 -0500

    deploy

--- ecommerce_rest/settings/base.py
+++ ecommerce_rest/settings/base.py
@@ -140,3 +140,6 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import django_heroku
+django_heroku.settings(locals())
